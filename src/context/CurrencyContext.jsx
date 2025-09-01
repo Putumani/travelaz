ebafaab46a0d3e057f78cfe9388aa84bd20cbbf7
Putumani <putumaninomzaza@gmail.com>
2025-09-01 23:37:23 +0200

    Fix useCurrency import in ComparisonCard.jsx

--- src/context/CurrencyContext.jsx
+++ src/context/CurrencyContext.jsx
@@ -1,3 +1,11 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
 export const CurrencyContext = createContext();
+
+export function useCurrency() {
+  const context = useContext(CurrencyContext);
+  if (!context) {
+    throw new Error('useCurrency must be used within a CurrencyProvider');
+  }
+  return context;
+}
