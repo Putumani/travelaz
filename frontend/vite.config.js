6c6ce124e11e98e1af398a7373b6ebd4682ab0b2
Putumani <putumaninomzaza@gmail.com>
2026-01-20 18:32:01 +0200

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -29,4 +29,31 @@ export default defineConfig({
       },
     },
   },
+  "rewrites": [
+    {
+      "source": "/(.*)",
+      "destination": "/index.html"
+    }
+  ],
+  "headers": [
+    {
+      "source": "/(.*)",
+      "headers": [
+        {
+          "key": "Cache-Control",
+          "value": "public, max-age=0, must-revalidate"
+        }
+      ]
+    },
+    {
+      "source": "/assets/(.*)",
+      "headers": [
+        {
+          "key": "Cache-Control",
+          "value": "public, max-age=31536000, immutable"
+        }
+      ]
+    }
+  ]
+
 })
